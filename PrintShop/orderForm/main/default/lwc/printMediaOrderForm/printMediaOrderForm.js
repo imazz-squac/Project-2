26e2ba8b9756089f67334cc6fea6f6ffad7f4da0
unknown <ryanmacmcdowell@gmail.com>
2022-01-18 22:08:55 -0500

    Commit6

--- PrintShop/orderForm/main/default/lwc/printMediaOrderForm/printMediaOrderForm.js
+++ PrintShop/orderForm/main/default/lwc/printMediaOrderForm/printMediaOrderForm.js
@@ -6,14 +6,15 @@ import COLOR_FIELD from '@salesforce/schema/PrintMedia__c.Color__c';
 import COVERJACKET_FIELD from '@salesforce/schema/PrintMedia__c.Cover_Jacket__c';
 import PAGEMATERIAL_FIELD from '@salesforce/schema/PrintMedia__c.PageMaterial__c';
 import PAGESIZE_FIELD from '@salesforce/schema/PrintMedia__c.PageSize__c';
-import RECORDTYPE_FIELD from '@salesforce/schema/PrintMedia__c.RecordTypeId';
+import COVERMATERIAL_FIELD from '@salesforce/schema/PrintMedia__c.Cover_Material__c';
+
 
 export default class RecordFormCreateExample extends LightningElement {
   
     @api PrintMedia__c;
     @api recordTypeId;
 
-    fields = [PAGEMATERIAL_FIELD, PAGESIZE_FIELD, BINDING_FIELD, COLOR_FIELD, COVERJACKET_FIELD];
+    fields = [PAGEMATERIAL_FIELD, PAGESIZE_FIELD, BINDING_FIELD, COLOR_FIELD, COVERJACKET_FIELD, COVERMATERIAL_FIELD];
 
     handleSuccess(event) {
         const evt = new ShowToastEvent({
@@ -22,5 +23,10 @@ export default class RecordFormCreateExample extends LightningElement {
             variant: 'success',
         });
         this.dispatchEvent(evt);
+    }
+
+    set recordType(id){
+        this.recordTypeId=id;
+        console.log(id);
     }
 }
